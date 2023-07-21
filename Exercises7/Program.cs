c8fca88e6bcbcd1c8b9c80de19a3779f1d9813ed
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-21 15:38:55 +0300

    Proje dosyası ekle.

---
+++ Exercises7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanıcıdan alınan sayinin sıfır olma durumunu if-else ile olusturun
+
+            int sayi;
+
+            Console.WriteLine("Lutfen bir asyi giriniz : ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi == 0)
+            {
+                Console.WriteLine(" Girmis oldugunuz sayi sıfır (0) ");
+            }
+            else if (sayi > 0)
+            {
+                Console.WriteLine("Girmis oldugunuz sayi pozitif ");
+            }
+            else
+            {
+                Console.WriteLine("Girmis oldugunuz sayi negatif");
+            }
+
+            Console.Read();
+        }
+    }
+}
